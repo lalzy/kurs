da5435de14d6e5bdf8233b4e4e2f44696d3624f8
skn <svenyk@gmail.com>
2025-01-22 12:56:03 +0100

    oppgave

--- selvstudie/Scrimba/people counter/index.js
+++ selvstudie/Scrimba/people counter/index.js
@@ -1,5 +1,19 @@
+let count = 0
+let saved = false
 function increment(){
-    var element = document.getElementById("count-el");
-    var value = parseInt(element.textContent)
-    element.textContent = ++value;
+    var element = document.getElementById("count-el");    
+    count = parseInt(element.textContent)
+    element.textContent = ++count;
+}
+
+function save(){
+    let doc = document.getElementById("save-el");
+    if(!doc.textContent.includes(count)){
+        if(saved){
+            doc.textContent += ` - ${count}`
+        }else{
+            saved = true
+            doc.textContent += `${count}`
+        }
+    }
 }
